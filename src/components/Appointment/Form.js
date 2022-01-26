56ffbdd988d633f7ca54639ddda8d606234ff805
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2022-01-26 00:38:10 -0500

    testing of the form, and editing it to spit out errors if no student/interviewer is selected when the form submits:

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function () {
     setStudent("");
@@ -15,8 +16,21 @@ export default function Form(props) {
     props.onCancel()
   }
 
-  const saveCapture = function () {
-    return props.onSave(student, interviewer)
+  // const saveCapture = function () {
+  //   return props.onSave(student, interviewer)
+  // }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    } else if (interviewer === null) {
+      setError("Pick the interviewer you want!")
+      return;
+    } else {
+      setError("");
+      return props.onSave(student, interviewer);
+    }
   }
 
   return (
@@ -30,7 +44,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -41,7 +57,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={saveCapture}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
